7e9b7a715967281e1a04c32c7d20a2776ca65598
raghavxope <akileshbabu1997@gmail.com>
2019-03-10 13:57:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/miniproject/REG.java
@@ -0,0 +1,29 @@
+package com.example.miniproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class REG extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_reg);
+    }
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+       // updateUI(currentUser);
+    }
+
+
+}
